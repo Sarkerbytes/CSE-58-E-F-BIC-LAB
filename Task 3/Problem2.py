159109dea54fc2237e75ea4a32879b4b6fe4cab9
Tushar Sarker <140781486+Sarkerbytes@users.noreply.github.com>
2026-06-16 16:08:47 +0600

    Add k-mer matching logic with mismatch tolerance
    Implement a k-mer matching algorithm with allowed mismatches.

--- Task 3/Problem2.py
+++ Task 3/Problem2.py
@@ -1,1 +1,19 @@
+kmers= input()
+str = input()
 
+d = int(input())
+
+szptr = len(kmers)
+sizestr = len(str)
+
+for i in range (0, sizestr) :
+    cnt = 0
+    idx = i
+    for j in range(0,szptr,1) :
+        if( kmers[j] != str[idx] ) :
+            cnt+=1
+        idx+=1
+    
+    if( cnt <= d ) :
+        print(i)
+    
